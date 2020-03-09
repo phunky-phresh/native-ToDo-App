df9c356981f5fcd832ab4214ba92eee2b7c75fef
ncooney94 <nick.cooney@gmail.com>
2020-03-09 23:39:40 +1100

    conditional, todo must be longer than 3 char, added alert

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Alert } from 'react-native';
 
 import Header from './components/header';
 import TodoItem from './components/todoItem';
@@ -20,12 +20,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos( (prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    })
+
+    if (text.length > 3) {
+      setTodos( (prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      })
+    } else {
+      Alert.alert('OOPS!', 'Todos need to be over 3 Chars long',[
+        {text: 'Understood'}
+      ])
+    }
+
   }
 
   return (
